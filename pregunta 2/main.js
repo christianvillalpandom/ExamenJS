3c1269fd4769042cf89f8078fb555352b2f94453
ChristianVillalpando <cristiano.011@hotmail.com>
2016-05-31 20:54:44 -0500

    Segundo Commit

---
+++ pregunta 2/main.js
@@ -0,0 +1,16 @@
+/*Pregunta 2 (1 ptos)
+Se pide una función que recibe un arreglo como parámetro y devuelva una copia del arreglo.
+Probar con un ejemplo sencillo que funcione.*/
+
+var startups = mostrars(
+    "FaceBook",
+    "Netflix",
+    "Tesla"
+	); 
+
+function mostrars (a,b,c) {
+
+	 alert("Empresa 1: " + a + ", Empresa 2: " + b + " y Empresa 3: " + c + ".")
+
+}
+
